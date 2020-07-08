d6d33f62bac945499d20207874a9f8f35a84c6ac
Thiago Iosolini <thiagoioscc@gmail.com>
2020-07-08 06:37:48 -0300

    Tipo entity class

--- src/application/main.java
+++ src/application/main.java
@@ -1,7 +1,11 @@
 package application;
 
+import model.entities.Tipo;
+
 public class main {
     public static void main(String[] args) {
-
+        Tipo tipo = new Tipo(1, "Guardanapo");
+        System.out.println(tipo);
     }
 }
+    
